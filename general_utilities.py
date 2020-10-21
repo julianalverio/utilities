38d9ddce7bb83742c00f86299e75f68875974d67
julianalverio <julian.a.alverio@gmail.com>
2020-10-21 12:39:54 -0400

    iterating

--- general_utilities.py
+++ general_utilities.py
@@ -18,10 +18,13 @@ def make_transmat(size, p=0.99):
     return transmat
 
 
-def write_mp4(frames, path, fps=5):
+def write_mp4(frames, path, fps=5, switch=True):
     shape = (500, 500)
     writer = cv2.VideoWriter(path, 0x7634706d, fps, shape)
     for frame in frames:
+        red = frame[:, :, 0].copy()
+        frame[:, :, 0] = frame[:, :, 2]
+        frame[:, :, 2] = red
         writer.write(frame)
     writer.release()
 
